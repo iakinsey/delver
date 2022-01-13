220c71abbacf3780a4621334b0f3174791c9475b
Ian Kinsey <ian@aikbix.com>
2022-01-13 00:26:39 -0500

    Language extractor test

---
+++ extractors/language_test.go
@@ -0,0 +1,40 @@
+package extractors
+
+import (
+	"testing"
+
+	"github.com/iakinsey/delver/types"
+	"github.com/iakinsey/delver/types/features"
+	"github.com/iakinsey/delver/types/message"
+	"github.com/stretchr/testify/assert"
+)
+
+var langScenarios = map[string]string{
+	"The quick brown fox jumps over the lazy dog.":              "en",
+	"你来自哪里？":                                                    "zh",
+	"¿Cómo se dice ‘concert’ en español?":                       "es",
+	"لِنَذْهَبْ إِلَى السِّيْنَمَا":                             "ar",
+	"Qu’est-ce que vous aimez faire pendant votre temps libre?": "fr",
+	"Можно заплатить кредитной карточкой?":                      "ru",
+}
+
+func TestLanguageExtractor(t *testing.T) {
+	extractor := NewLanguageExtractor()
+	meta := message.FetcherResponse{}
+
+	for text, expectedLang := range langScenarios {
+		textContent := features.TextContent(text)
+
+		composite := types.CompositeAnalysis{
+			TextContent: textContent,
+		}
+
+		lang, err := extractor.Perform(nil, meta, composite)
+		actualLang := lang.(features.Language).Name
+
+		assert.NoError(t, err)
+		assert.NotNil(t, lang)
+		assert.IsType(t, features.Language{}, lang)
+		assert.Equal(t, expectedLang, actualLang)
+	}
+}
