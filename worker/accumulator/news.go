1b416be817c8f95485426a66d25b10bf5e4ad3c6
Ian Kinsey <ian@aikbix.com>
2022-01-24 01:51:28 -0500

    Make news accumulator

--- worker/accumulator/news.go
+++ worker/accumulator/news.go
@@ -1,1 +1,19 @@
 package accumulator
+
+import (
+	"github.com/iakinsey/delver/types"
+	"github.com/iakinsey/delver/worker"
+)
+
+type newsAccumulator struct {
+}
+
+func NewNewsAccumulator() worker.Worker {
+	return &newsAccumulator{}
+}
+
+func (s *newsAccumulator) OnMessage(msg types.Message) (interface{}, error) {
+	return nil, nil
+}
+
+func (s *newsAccumulator) OnComplete() {}
