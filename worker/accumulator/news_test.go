bc184bfbac6a84bea5324dc60797fa1fbeddcef5
Ian Kinsey <ian@aikbix.com>
2022-01-27 22:26:45 -0500

    News accumulator works

---
+++ worker/accumulator/news_test.go
@@ -0,0 +1,51 @@
+package accumulator
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/iakinsey/delver/types"
+	"github.com/iakinsey/delver/types/features"
+	"github.com/iakinsey/delver/types/message"
+	"github.com/iakinsey/delver/util/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewsAccumulator(t *testing.T) {
+	paths := testutil.SetupWorkerQueueFolders("NewsAccumulatorTest")
+
+	defer testutil.TeardownWorkerQueueFolders(paths)
+
+	queues := testutil.CreateQueueTriad(paths)
+	newsQueue := queues.Outbox
+	accumulator := NewNewsAccumulator(newsQueue)
+	composite, _ := json.Marshal(message.CompositeAnalysis{
+		FetcherResponse: message.FetcherResponse{
+			FetcherRequest: message.FetcherRequest{
+				URI: "http://test.com/example",
+			},
+		},
+		URIs: features.URIs{
+			"http://test.com",
+			"http://example.com",
+		},
+	})
+	msg := types.Message{
+		ID:          "0-0-0-TestName",
+		MessageType: types.CompositeAnalysisType,
+		Message:     json.RawMessage(composite),
+	}
+
+	result, err := accumulator.OnMessage(msg)
+
+	assert.NoError(t, err)
+	assert.IsType(t, types.MultiMessage{}, result)
+
+	multiMessage := result.(types.MultiMessage)
+
+	assert.Len(t, multiMessage.Values, 1)
+
+	for _, value := range multiMessage.Values {
+		assert.IsType(t, message.FetcherRequest{}, value)
+	}
+}
