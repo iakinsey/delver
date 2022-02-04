ec00fba3fff33067a725344b266c51267ef4648f
Ian Kinsey <ian@aikbix.com>
2022-02-04 02:19:23 -0500

    Add dfs basic publisher test

---
+++ worker/publisher/dfs_basic_test.go
@@ -0,0 +1,62 @@
+package publisher
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/iakinsey/delver/types"
+	"github.com/iakinsey/delver/types/message"
+	"github.com/iakinsey/delver/util"
+	"github.com/iakinsey/delver/util/maps"
+	"github.com/iakinsey/delver/util/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDfsBasic(t *testing.T) {
+	paths := testutil.SetupWorkerQueueFolders("DfsBasic")
+
+	defer testutil.TeardownWorkerQueueFolders(paths)
+
+	queues := testutil.CreateQueueTriad(paths)
+	urlStorePath := util.NewTempPath("dfsBasicUrlStore")
+	visitedDomainsPath := util.NewTempPath("dfsBasicVisitedDomains")
+	rotateAfter := 1 * time.Millisecond
+	mapper := maps.NewMultiHostMap(urlStorePath)
+	urls := []string{
+		"http://example.com/1",
+		"http://example.com/2",
+		"http://example.com/3",
+		"http://example.com/4",
+	}
+
+	for _, u := range urls {
+		req := message.FetcherRequest{
+			URI: u,
+		}
+
+		if val, err := json.Marshal(req); err != nil {
+			log.Fatalf("error preparing request: %s", err)
+		} else {
+			assert.NoError(t, mapper.Set([]byte(u), val))
+		}
+	}
+
+	mapper.Close()
+
+	publisher := NewDfsBasicPublisher(
+		queues.Inbox,
+		queues.Outbox,
+		urlStorePath,
+		visitedDomainsPath,
+		rotateAfter,
+	)
+	out, err := publisher.OnMessage(types.Message{})
+
+	assert.Nil(t, out)
+	assert.NoError(t, err)
+	testutil.AssertFolderSize(t, paths.Outbox, len(urls))
+
+	
+}
