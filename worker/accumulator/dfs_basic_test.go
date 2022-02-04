d31948140fbd1fd58fa40896e555fcc5397935a2
Ian Kinsey <ian@aikbix.com>
2022-02-04 00:19:24 -0500

    Basic DFS passes all tests

--- worker/accumulator/dfs_basic_test.go
+++ worker/accumulator/dfs_basic_test.go
@@ -70,4 +70,41 @@ func TestDfsBasic(t *testing.T) {
 	assert.Len(t, mm2.Values, 0)
 }
 
-func TestDfsBasicMaxDepthExceeded(t *testing.T) {}
+func TestDfsBasicMaxDepthExceeded(t *testing.T) {
+	maxDepth := 1
+	urlStorePath := util.NewTempPath("urlStore")
+	visitedUrlsPath := util.NewTempPath("visitedUrls")
+
+	defer os.Remove(visitedUrlsPath)
+	defer os.RemoveAll(urlStorePath)
+
+	accumulator := NewDfsBasicAccumulator(urlStorePath, visitedUrlsPath, maxDepth)
+	msg1, _ := types.NewMessage(message.CompositeAnalysis{
+		FetcherResponse: message.FetcherResponse{
+			FetcherRequest: message.FetcherRequest{
+				URI:   "http://example.com",
+				Host:  "example.com",
+				Depth: 1,
+			},
+		},
+		URIs: features.URIs{
+			"http://example.com/1",
+			"http://example.com/2",
+			"http://example.com/3",
+			"http://example.com/4",
+			"http://old.example.com/5",
+			"http://non.com",
+		},
+	}, types.CompositeAnalysisType)
+
+	out, err := accumulator.OnMessage(msg1)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, out)
+	assert.IsType(t, types.MultiMessage{}, out)
+
+	mm, ok := out.(types.MultiMessage)
+
+	assert.True(t, ok)
+	assert.Len(t, mm.Values, 0)
+}
